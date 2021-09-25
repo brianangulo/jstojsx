7c9a4bd3191a30321e5535ef61161e330d4e05ef
Brian Angulo <brianangulo96@gmail.com>
2021-09-24 22:55:02 -0500

    Began thinking out searcher script

---
+++ src/searcher.ts
@@ -0,0 +1,8 @@
+import { readdirSync, rename } from "fs";
+import { cwd } from "process";
+
+//extract folder paths 
+
+//extract folder paths all the way to none remaining
+
+//call translator for every single path
